23c2d894a78830573b7c57ad44a806fe3bf2bb69
WeslleySantosln <weslleys.santana299@gmail.com>
2024-01-08 10:41:52 -0300

    update

---
+++ 01_class/01_tipos de dados/date_type.py
@@ -0,0 +1,30 @@
+# --------- TIPOS DE DADOS ----------- #
+print("# --------- TIPOS DE DADOS ----------- #")
+
+print(11 +20 + 1000)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Python")
+
+int()
+float()
+bool()
+bool()
+str()
+
+# --------- DIR E HELP ----------- #
+print("# --------- DIR E HELP ----------- #")
+import math
+
+#dir(1)
+#help(math)
+
+# --------- VARIAVEIS E CONSTANTES ----------- #
+print("# --------- VARIAVEIS E CONSTANTES ----------- #")
+
+NOME = "Weslley"
+idade = 29
+
+print("Meu nome é: " + NOME, ",Tenho ", idade, "anos.")
+
