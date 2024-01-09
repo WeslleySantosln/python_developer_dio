5a3e7a79835e3dbaa3a7ce117f0e92a8036a9111
WeslleySantosln <weslleys.santana299@gmail.com>
2024-01-08 23:03:38 -0300

    update

--- 01_class/01_tipos de dados/date_type.py
+++ 01_class/01_tipos de dados/date_type.py
@@ -28,3 +28,37 @@ idade = 29
 
 print("Meu nome é: " + NOME, ",Tenho ", idade, "anos.")
 
+
+# --------- TIPOS DE CONVENÇÕES ----------- #
+print("# --------- TIPOS DE CONVENÇÕES ----------- #")
+
+fl = idade / 2
+
+print(fl)
+
+fl = idade // 2
+
+print(fl)
+
+st = str(f"String: {fl}")
+
+print(st)
+
+print(int(112.211223))
+
+str = "10.10"
+
+fl = idade / float(str)
+
+print(fl)
+
+
+# --------- INPUT E OUTPUT  ----------- #
+print("# --------- INPUT E OUTPUT  ----------- #")
+
+nome = input("informe seu nome: ")
+idade = input("informe sua idade: ")
+
+print(nome, idade)
+print(nome, idade, end=". . . \n")
+print(nome, idade, sep=" * ")
