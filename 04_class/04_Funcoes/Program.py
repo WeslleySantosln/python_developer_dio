7ff2b23a29c417f9e34f4bc8dc2f94b63b8a80c2
Weslleysantosln <weslleys.santana299@gmail.com>
2024-02-16 10:52:52 -0300

    update

---
+++ 04_class/04_Funcoes/Program.py
@@ -0,0 +1,12 @@
+#Crie uma função, crie uma função com args e kwargs, crie uma função que recebe uma função como argumento 
+
+
+
+#retorne 1 valor, retorne 2 valores, 
+
+
+
+
+n, k = map(int, input().split())
+
+print(k,n)
